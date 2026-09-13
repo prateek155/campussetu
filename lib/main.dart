7d67ddb68539327ccedf0a076c549c3fe3dabf74
prateek155 <prateekagrawal120@gmail.com>
2026-09-13 22:50:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   // Lock orientation to portrait for mobile
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitDown, 
   ]);
 
   // Status bar style
